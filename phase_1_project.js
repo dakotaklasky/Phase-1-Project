98795173b29dd545eec4880fc9bddaf414b6a6f4
Dakota <dakota.klasky@gmail.com>
2024-07-29 16:00:09 -0400

    add department filter

--- phase_1_project.js
+++ phase_1_project.js
@@ -5,7 +5,7 @@ function getRandomIndex(artObjectIDs){
 
 //Test if the index of the given array of object Ids returns a valid object with an image URL
 function testIfImage(artObjectIDs,randomIndex){
-    //console.log(randomIndex)
+    console.log(artObjectIDs[randomIndex])
     const imgDescription = document.getElementById('description')
     fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artObjectIDs[randomIndex]}`)
         .then(response => {
@@ -23,58 +23,87 @@ function testIfImage(artObjectIDs,randomIndex){
                     //display image URL
                     else{
                         const primaryImg = document.getElementById('todays-image')
+                        const imgLink = document.getElementById('img-link')
                         primaryImg.src = json.primaryImage
                         primaryImg.style.height = '500px'
+                        imgLink.href = json.objectURL
 
                         const imgDescription = document.getElementById('description')
                         imgDescription.textContent = "Image Description:"
-                        const descriptionArray = [`Title: ${json.title}`,`Department: ${json.department}`,`Culture: ${json.culture}`,
-                            `Period: ${json.period}`,`Artist: ${json.artistDisplayName}`,`Date: ${json.objectDate}`,`Medium: ${json.medium}`,
-                            `Dimensions: ${json.dimensions}`]
+                        const descriptionArray = [`Title: ${json.title}`,`Department: ${json.department}`,
+                            `Culture: ${json.culture}`, `Country: ${json.country}`,`Period: ${json.period}`,
+                            `Artist: ${json.artistDisplayName}`,`Date: ${json.objectDate}`,
+                            `Medium: ${json.medium}`,`Dimensions: ${json.dimensions}`]
                         
                         descriptionArray.forEach(data =>{
                             const liElement = document.createElement('li')
                             liElement.textContent = data
                             imgDescription.appendChild(liElement)
                         })
-                        
-                        
-            
+                    
                         const linkElement = document.createElement('a')
-                        linkElement.textContent = json.objectWikidata_URL
-                        linkElement.href = json.objectWikidata_URL
+                        linkElement.textContent = json.objectURL
+                        linkElement.href = json.objectURL
                         imgDescription.appendChild(linkElement)
-                        
-
                     }
                 })
             }
         })
 }
 
-function main(){
-    
-    const imgButton = document.getElementById('generate-img')
-    imgButton.addEventListener('click', () => {
-        fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects")
-        .then(response => response.json())
-        .then(json => {
-            testIfImage(json.objectIDs,getRandomIndex(json.objectIDs))
-        })
+function fetchData(resource){
+    fetch(resource)
+    .then(response => response.json())
+    .then(json => {
+        testIfImage(json.objectIDs,getRandomIndex(json.objectIDs))
     })
+}
+
+function main(){
+
+    const showArt = document.getElementById('description-input')
+    showArt.addEventListener('submit', (event) =>{
+        event.preventDefault()
+        const dropdownElement = document.getElementById('departments')
+        if(dropdownElement.value === "0"){
+            fetchData("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+        }
+        else{
+            fetchData(`https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=${dropdownElement.value}&q=cat`)
+        }
+    })
+
+
+
+    
+
+
 }
 
 main()
 
 
 
-//Requirements for assessment at 
-//* At least 1 variable declared using either the const or let keyword.
-//* At least 1 named function declared, and at least 1 anonymous function or arrow function used.
-//* At least 1 array and 1 object accessible either within your project code or retrieved from your db.json or external API using the fetch() function.
-//* Write the code to search for at least 1 element from the DOM, store it into a variable, and change at least 1 property for the element such as textContent or src.
-//* Write the code to add at least 1 event listener to an element, to make the element listen for an event such as click or submit and execute code in response to the event.
-//* Write the code to call the fetch() function to make a GET request to a server (either db.json server or external API) to retrieve data from the server.
+
+// const departments = ["American Decorative Arts","Ancient Near Eastern Art","Arms and Armor", 
+// "Arts of Africa, Oceania, and the Americas","Asian Art","The Cloisters",
+// "The Costume Institute","Drawings and Prints","Egyptian Art","European Paintings",
+// "European Sculpture and Decorative Arts","Greek and Roman Art","Islamic Art",
+// "The Robert Lehman Collection","The Libraries","Medieval Art","Musical Instruments",
+// "Photographs","Modern Art"]
 
 
 
+
+//To Do
+//submit form event listeners
+//Drop down event listeners (change)
+//remove empty description elements or input unknown
+//formatting
+
+//create dropdown
+//when element is changed in dropdown then the object id link is selected from there
+//when it is loading add an element to say so
+
+//go back to previous button
+//mouseover
